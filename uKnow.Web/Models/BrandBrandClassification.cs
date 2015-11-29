84ce09222f14a1cce1ee6b6569e30c8ed3bdad0b
hechaner <hechaner@126.com>
2015-11-29 22:53:12 +0800

    Brand and BrandClassification relation model

---
+++ uKnow.Web/Models/BrandBrandClassification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace uKnow.Web.Models
+{
+    public class BrandBrandClassification
+    {
+        [Key]
+        public virtual int ID { get; set; }
+
+        public virtual int BrandID { get; set; }
+
+        [ForeignKey("BrandID")]
+        public virtual Brand Brand { get; set; }
+
+        public virtual int BrandClassificationID { get; set; }
+
+        [ForeignKey("BrandClassificationID")]
+        public virtual BrandClassification BrandClassification { get; set; }
+    }
+}
