a7f5870a4d27f2d3a8ecb84f2f810c0a64cea3dc
hechaner <hechaner@126.com>
2015-12-01 09:33:16 +0800

    db init

--- uKnow.Web/Global.asax.cs
+++ uKnow.Web/Global.asax.cs
@@ -6,6 +6,9 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using uKnow.Web.Models;
+using System.Data.Entity.Infrastructure;
 
 namespace uKnow.Web
 {
@@ -18,6 +21,34 @@ namespace uKnow.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            new DBInitializer();
+        }
+
+
+    }
+
+    public class DBInitializer
+    {
+        public DBInitializer()
+        {
+            Database.SetInitializer<uKnowDataContext>(null);
+
+            try 
+            {
+                using (var context = new uKnowDataContext())
+                {
+                    if (!context.Database.Exists())
+                    {
+                        // Create the SimpleMembership database without Entity Framework migration schema
+                        ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+            }
         }
     }
 }
